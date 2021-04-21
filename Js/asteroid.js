cf09ec9cbf253d6dea9e9fc1c7d0111307fb1882
somePythonProgrammer <leenagajbhiye1003@gmail.com>
2021-04-21 13:47:17 +0530

    Another commit

---
+++ Js/asteroid.js
@@ -0,0 +1,10 @@
+class Asteroid{
+    constructor(){
+        this.posX = Math.round(random(-windowWidth,windowWidth))
+        this.posX = Math.round(random(-windowWidth,windowWidth))
+        this.width = Math.round(random(10,56))
+        this.height = Math.round(random(10,56))
+    }
+
+    
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
+var engine, world;
 
 function setup(){
     canvas = createCanvas(windowWidth-0,windowHeight-0)
+    engine = Engine.create();
+    world = engine.world;
 }
 
 function preload(){

fdb0e46d7dd41969bd25451faf380a8b8bf73833
somePythonProgrammer <leenagajbhiye1003@gmail.com>
2021-04-21 15:46:45 +0530

    Another commit

--- Js/asteroid.js
+++ Js/asteroid.js
@@ -1,10 +1,21 @@
-class Asteroid{
-    constructor(){
-        this.posX = Math.round(random(-windowWidth,windowWidth))
-        this.posX = Math.round(random(-windowWidth,windowWidth))
-        this.width = Math.round(random(10,56))
-        this.height = Math.round(random(10,56))
+class Asteroid extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+        }
+      this.width = random(30,40)
+      this.height = random(30,40)
+      this.body = Bodies.rectangle(x, y, this.width, options);
+      this.image = loadImage("Images/Asteroid.png");
+      this.rotation=random(-5,5)
+      Matter.Body.applyForce( this.body, {x: this.body.position.x, y: this.body.position.y}, {x: 0.05, y: 0})
     }
-
-    
+  
+    display(){
+      super.display();
+      Matter.Body.rotate(this.body,this.rotation)
+    }
 }
--- sketch.js
+++ sketch.js
@@ -3,18 +3,23 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-
-function setup(){
-    canvas = createCanvas(windowWidth-0,windowHeight-0)
-    engine = Engine.create();
-    world = engine.world;
-}
+var asteroids = []
 
 function preload(){
 
 }
 
+function setup(){
+    canvas = createCanvas(windowWidth-0,windowHeight-0)
+    engine = Engine.create();
+    world = engine.world;
+    world.gravity.y = 0
+    asteroid = new Asteroid(300,300)
+    Engine.run(engine)
+}
+
 function draw(){
     resizeCanvas(windowWidth-0,windowHeight-0)
     background('black')
+    asteroid.display()
 }  
